6f2c6505a730994acf119303352244c06d8ac6e1
Gabor <ghalasz1988@gmail.com>
2021-03-12 17:32:20 +0000

    fixed provider  issues

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -26,10 +22,13 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('client');
   });
 
+  //This test isn't passing why?
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('client app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'client app is running!'
+    );
   });
 });
