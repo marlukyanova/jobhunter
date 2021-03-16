04678c800c85975670582dc75c73509ef625a184
Shaun Martin <shaun.martin@gmx.de>
2021-03-16 10:27:38 +0100

    testing update

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title JobHunter', () => {
+  it('should have JobHunter as title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('JobHunter');
